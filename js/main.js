17a8902fb320e5c2fee7798fa519fc5c86982435
ilkkamtk <ilkka.kylmaniemi@metropolia.fi>
2019-02-11 21:14:29 +0200

    watchPosition

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 // Asetukset paikkatiedon hakua varten (valinnainen)
+const map = L.map('map');
+
 const options = {
   enableHighAccuracy: true,
   timeout: 5000,
@@ -14,17 +16,24 @@ function success(pos) {
   console.log(`Latitude : ${crd.latitude}`);
   console.log(`Longitude: ${crd.longitude}`);
   console.log(`More or less ${crd.accuracy} meters.`);
+  naytaKartta(crd);
+  lisaaMarker(crd);
+}
 
+function naytaKartta(crd) {
   // Käytetään leaflet.js -kirjastoa näyttämään sijainti kartalla (https://leafletjs.com/)
-  const map = L.map('map').setView([crd.latitude, crd.longitude], 13);
+  map.setView([crd.latitude, crd.longitude], 13);
   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
   }).addTo(map);
+}
 
+function lisaaMarker(crd) {
   L.marker([crd.latitude, crd.longitude]).addTo(map)
   .bindPopup('Olen tässä.')
   .openPopup();
 }
+
 
 // Funktio, joka ajetaan, jos paikkatietojen hakemisessa tapahtuu virhe
 function error(err) {
@@ -32,4 +41,4 @@ function error(err) {
 }
 
 // Käynnistetään paikkatietojen haku
-navigator.geolocation.getCurrentPosition(success, error, options);
+navigator.geolocation.watchPosition(success, error, options);
